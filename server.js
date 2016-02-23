63618d9f9df43770eb45894a0a896e846a7fd31f
Gabriel N. Schenker <gnschenker@gmail.com>
2016-02-22 21:17:01 -0600

    Using moment

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http')
+var moment = require('moment');
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Node JS says "Hello World"\n');
+  var dt = moment().format();
+  res.end('Node JS says "Hello World"\n' + dt);
 }).listen(port);
