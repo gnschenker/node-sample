499e504008d332db53c623cf763a463426da6546
Gabriel N. Schenker <gnschenker@gmail.com>
2016-03-22 15:07:19 -0500

    Added products endpoint

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.get('/', function (req, res) {
   res.send('Express JS says "Hello ' + name + '"!<br>' + dt);
 });
 
+app.get('/products', function (req, res){
+  var products = {
+    {"id": 1, "name": "Apples"}
+    {"id": 2, "name": "Pears"}
+    {"id": 3, "name": "Lemons"}
+  };
+  res.send(products);
+});
+
 var port = process.env.PORT || 1337;
 app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", port, app.settings.env);
